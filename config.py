989f50badd5eca66b1dd691298121df1c5a7c366
anination2024 <skypictures19@gmail.com>
2024-10-05 11:05:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = 17193413
+api_id = 28509727
 
-api_hash = "5e50e6380252d9d487093bae4171bf91"
+api_hash = "07d6e9be440c5176552d905c1206602d"
 
-bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
+bot_token = "7931617358:AAF-mEhifUv0cfBqcLDFnU-lsFogAT9mV-A"
 
 GROUP_TAG = "RC"
 
-sudo_users = [1606667548]
+sudo_users = [6041853726]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
